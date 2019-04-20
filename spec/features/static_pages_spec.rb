36be0f45e398d5ca2c3b0c0fe0a621b16f8574f7
Andrew De Abreu Ruiz <andrewabreur@gmail.com>
2019-04-20 18:13:38 +0100

    Add have_link matchers in feature specs of StaticPages
    Add temporary jumbotron

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -4,7 +4,13 @@ RSpec.feature "StaticPages", type: :feature do
   
 	scenario "viewing the home page" do
 		visit root_path
+
 		expect(page).to have_title("Inicio | Box2Share")
+		
+		expect(page).to have_link("Home", :href => root_path)
+		expect(page).to have_link("Pricing", :href => '#')	
+		expect(page).to have_link("Sign In", :href => '#')	
+		expect(page).to have_link("Sign Up", :href => '#')
 	end
 
 end
