5492f3529d5203d4b6b465131bbd27a8f014d1b1
Andrew De Abreu Ruiz <andrewabrer@gmail.com>
2019-04-21 20:06:11 +0100

    Add admin role to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_21_184345) do
+ActiveRecord::Schema.define(version: 2019_04_21_190424) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_04_21_184345) do
     t.string "unconfirmed_email"
     t.string "name"
     t.string "last_name"
+    t.boolean "admin", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
