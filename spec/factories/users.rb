38629464d4e254348cd241eac0dbb4cf2e92cf47
Andrew De Abreu Ruiz <andrewabrer@gmail.com>
2019-04-25 12:19:56 +0100

    Fix factory_bot.rb error and delete users.rb factory file

--- spec/factories/users.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    name {Faker::Name.name}
-    last_name {Faker::Name.last_name}
-    email {Faker::Internet.email}
-    password { 'foobar' }
-    password_confirmation {'foobar'}
-  end
-end
