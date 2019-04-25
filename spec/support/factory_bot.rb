4e5d7b89e7d0bdb53041fa5dbe5d41e28e202036
Andrew De Abreu Ruiz <andrewabreur@gmail.com>
2019-04-25 12:11:31 +0100

    Modify spec/support/factory_bot.rb file
    Add in the support file of factorybot that uses database_cleaner and that proves that the factories are working.

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,4 +1,15 @@
 RSpec.configure do |config|
-  config.include FactoryBot::Syntax::Methods	
+  config.include FactoryBot::Syntax::Methods
+
+	config.before(:suite) do
+    begin
+      DatabaseCleaner.start
+      # Test factories in spec/factories are working.
+      FactoryGirl.lint
+    ensure
+      DatabaseCleaner.clean
+    end
+  end
+
 end
 
