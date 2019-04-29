67138c50bd0d802596885fac98d8f63f83b7f4ac
Andrew De Abreu Ruiz <andrewabreur@gmail.com>
2019-04-29 19:48:13 +0100

    Change user factory
    Add transient options

---
+++ spec/factories/users.rb
@@ -0,0 +1,21 @@
+FactoryBot.define do
+	
+	factory :user do
+
+		transient do
+			skip_confirmation { true }
+		end
+
+		name { 'Name' }
+		last_name { 'Last Name' }
+		sequence(:email) { |n| "test#{n}@example.com" }
+		password { 'foobar' }
+		password_confirmation { 'foobar' }
+
+		before(:create) do |user, evaluator|
+			user.skip_confirmation! if evaluator.skip_confirmation
+		end
+			
+	end
+
+end
