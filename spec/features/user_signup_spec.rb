62778f153d4609bb90169941559c059a301833e7
Andrew De Abreu Ruiz <andrewabreur@gmail.com>
2019-04-29 20:19:11 +0100

    Refactor: Extract sign up and sign in steps into a function

--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -8,26 +8,18 @@ RSpec.feature "User sign up", type: :feature do
   	click_link I18n.t('pages.header.navigation_links.signup')
   	expect(current_path).to eq(new_user_registration_path)
 
-  	fill_in "Name", with: "test-name"
-  	fill_in "Last name", with: "test-last-name"
-  	fill_in "Email", with: "test@domain.com"
-  	fill_in "Password", with: "test-password"
-  	fill_in "Password confirmation", with: "test-password"
-
-  	click_button I18n.t('devise.registrations.new.sign_up')
+    sign_up_with "test-name", "test-last-name", "test@example.com", "test-password", "test-password"
 
   	expect(current_path).to eq root_path
   	expect(page).to have_content(I18n.t('devise.registrations.signed_up_but_unconfirmed'))
 
-  	open_email "test@domain.com", with_subject: I18n.t('devise.mailer.confirmation_instructions.subject')
+  	open_email "test@example.com", with_subject: I18n.t('devise.mailer.confirmation_instructions.subject')
   	visit_in_email I18n.t('devise.mailer.confirmation_instructions.action')
 
   	expect(current_path).to eq new_user_session_path
   	expect(page).to have_content I18n.t('devise.confirmations.confirmed')
 
-  	fill_in "Email", with: "test@domain.com"
-  	fill_in "Password", with: "test-password"
-  	click_button I18n.t('devise.sessions.new.sign_in')
+    sign_in_with "test@example.com", "test-password"
 
   	expect(current_path).to eq root_path
   	expect(page).to have_content I18n.t('devise.sessions.signed_in')
@@ -52,12 +44,7 @@ RSpec.feature "User sign up", type: :feature do
 
   	scenario "with incorrect password confirmation" do
   		
-  		fill_in "Name", with: "test-name"
-  		fill_in "Last name", with: "test-last-name"
-      fill_in "Email", with: "test@domain.com"
-      fill_in "Password", with: "test-password"
-      fill_in "Password confirmation", with: "not-test-password"
-      click_button I18n.t('devise.registrations.new.sign_up')
+      sign_up_with "test-name", "test-last-name", "test@example.com", "test-password", "not-test-password"
 
       expect(page).to have_content I18n.t('errors.messages.not_saved.one')
 
@@ -67,12 +54,7 @@ RSpec.feature "User sign up", type: :feature do
 
       create(:user, email: "test@example.com")
 
-      fill_in "Name", with: "test-name"
-      fill_in "Last name", with: "test-last-name"
-      fill_in "Email", with: "test@example.com"
-      fill_in "Password", with: "test-password"
-      fill_in "Password confirmation", with: "test-password"
-      click_button I18n.t('devise.registrations.new.sign_up')
+      sign_up_with "test-name", "test-last-name", "test@example.com", "test-password", "test-password"
 
       expect(page).to have_content I18n.t('errors.messages.not_saved.one')
 
@@ -80,12 +62,7 @@ RSpec.feature "User sign up", type: :feature do
 
     scenario "with invalid email" do
 
-      fill_in "Name", with: "test-name"
-      fill_in "Last name", with: "test-last-name"
-      fill_in "Email", with: "invalid-email-for-testing"
-      fill_in "Password", with: "test-password"
-      fill_in "Password confirmation", with: "test-password"
-      click_button I18n.t('devise.registrations.new.sign_up')
+      sign_up_with "test-name", "test-last-name", "invalid-email-for-testing", "test-password", "test-password"
 
       expect(page).to have_content I18n.t('errors.messages.not_saved.one')    
 
@@ -96,12 +73,7 @@ RSpec.feature "User sign up", type: :feature do
       min_password_length = 6
       too_short_password = "p" * (min_password_length - 1)
 
-      fill_in "Name", with: "test-name"
-      fill_in "Last name", with: "test-last-name"
-      fill_in "Email", with: "test@example.com"
-      fill_in "Password", with: too_short_password
-      fill_in "Password confirmation", with: too_short_password
-      click_button I18n.t('devise.registrations.new.sign_up')
+      sign_up_with "test-name", "test-last-name", "test@example.com", too_short_password, too_short_password
 
       expect(page).to have_content I18n.t('errors.messages.not_saved.one')
 
@@ -117,6 +89,22 @@ RSpec.feature "User sign up", type: :feature do
       expect(page).to have_field("Email", with: "", type: "email")
       expect(find_field("Password", type: "password").value).to be_nil
       expect(find_field("Password confirmation", type: "password").value).to be_nil          
+    end
+
+    def sign_up_with(name, last_name, email, password, password_confirmation)
+      fill_in "Name", with: name
+      fill_in "Last name", with: last_name
+      fill_in "Email", with: email
+      fill_in "Password", with: password
+      fill_in "Password confirmation", with: password_confirmation
+
+      click_button I18n.t('devise.registrations.new.sign_up')      
+    end
+
+    def sign_in_with(email, password)
+      fill_in "Email", with: email
+      fill_in "Password", with: password
+      click_button I18n.t('devise.sessions.new.sign_in')
     end
 
 end
