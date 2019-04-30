ba8c33488dff026db202391907bfcf2433bdd8fe
Andrew De Abreu Ruiz <andrewabrer@gmail.com>
2019-04-30 18:27:01 +0100

    Change host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => "boxtoshare.herokuapp.com"}
+  config.action_mailer.default_url_options = { :host => "box2share.herokuapp.com"}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_options = { from: "noreply@example.com" }
   config.action_mailer.smtp_settings = {
