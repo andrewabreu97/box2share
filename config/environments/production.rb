b4b5848a4024ed70100512860385f13102682463
Andrew De Abreu Ruiz <andrewabrer@gmail.com>
2019-04-30 18:15:21 +0100

    Change ENV mailer in mailer production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,8 +100,8 @@ Rails.application.configure do
    address: "smtp.sendgrid.net",
    port: 587,
    domain: "heroku.com",
-   user_name: Rails.application.secrets.smtp_sendgrid_username,
-   password: Rails.application.secrets.smtp_sendgrid_password,
+   user_name: ENV['SENDGRID_USERNAME'],
+   password: ENV['SENDGRID_PASSWORD'],
    authentication: "plain",
    enable_starttls_auto: true
   }
