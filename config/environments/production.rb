2642b94701ea5e3dd320976d740370d762738fc8
Andrew De Abreu Ruiz <andrewabrer@gmail.com>
2019-05-01 10:09:07 +0100

    Put temporal SMTP Gmail config in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,17 +93,33 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => "box2share.herokuapp.com"}
+  # config.action_mailer.default_url_options = { :host => "box2share.herokuapp.com"}
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.default_options = { from: "noreply@example.com" }
+  # config.action_mailer.smtp_settings = {
+  #  address: "smtp.sendgrid.net",
+  #  port: 587,
+  #  domain: "heroku.com",
+  #  user_name: ENV['SENDGRID_USERNAME'],
+  #  password: ENV['SENDGRID_PASSWORD'],
+  #  authentication: "plain",
+  #  enable_starttls_auto: true
+  # }
+
+  config.action_mailer.default_url_options = { :host => "box2share.herokuapp.com" }
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_options = { from: "noreply@example.com" }
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_options = { from: "noreply@gmail.com" }
   config.action_mailer.smtp_settings = {
-   address: "smtp.sendgrid.net",
-   port: 587,
-   domain: "heroku.com",
-   user_name: ENV['SENDGRID_USERNAME'],
-   password: ENV['SENDGRID_PASSWORD'],
-   authentication: "plain",
-   enable_starttls_auto: true
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_USER'],
+    password: ENV['GMAIL_PASSWORD'],
+    openssl_verify_mode: "none"
   }
 
 end
