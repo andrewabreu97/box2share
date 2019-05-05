80236a86386dd00c845dabaacc7d08e6e9f4237b
Andrew De Abreu Ruiz <andrewabrer@gmail.com>
2019-05-05 10:47:20 +0100

    Add STRIPE_JS_HOST enviroment variable

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,3 @@
 Stripe.api_key = Rails.application.secrets.stripe_secret_key
 raise "Missing Stripe API Key" unless Stripe.api_key
+STRIPE_JS_HOST = "https://js.stripe.com".freeze unless defined? STRIPE_JS_HOST
