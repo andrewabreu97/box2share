757b297ae1290ad9b80f08f32aec81c2ae3bc33f
Andrew De Abreu Ruiz <andrewabrer@gmail.com>
2019-05-05 16:09:49 +0100

    Add resources to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'plans/index'
+  resources :plans
+  resources :payments
+  resources :subscriptions
+  resource :subscription_cart
   devise_for :users, :controllers => { registrations: 'registrations' }
   root 'static_pages#home'
 end
