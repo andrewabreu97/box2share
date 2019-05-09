a8c63c0a22264d685fadd5271892b15b79e0172d
Andrew De Abreu Ruiz <andrewabrer@gmail.com>
2019-05-09 15:44:34 +0100

    Add validation image format in file field

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
  	validates :name, presence: true
  	validates :last_name, presence: true
   validates_presence_of :avatar
