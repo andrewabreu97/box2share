60a6e68ccd1318ee6ae369810f6b275e3c8d0c0a
Andrew De Abreu Ruiz <andrewabrer@gmail.com>
2019-05-09 12:41:23 +0100

    Add mount_upload and avatar validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ApplicationRecord
+
+  mount_uploader :avatar, AvatarUploader
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
  	validates :name, presence: true
  	validates :last_name, presence: true
+  validates_presence_of :avatar
+  validates_integrity_of :avatar
+  validates_processing_of :avatar
 
   has_many :subscriptions
 
