0d70960aeae88ac88f87585e775f8c2e65d5c382
Andrew De Abreu Ruiz <andrewabrer@gmail.com>
2019-05-18 14:28:48 +0100

    Refactor routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,21 @@
 Rails.application.routes.draw do
-  get 'subscriptions/destroy'
-  resources :plans
-  resources :payments
-  resources :subscriptions, path_names: { new: 'new/:plan_id' }
+
   devise_for :users, :controllers => { registrations: 'registrations' }
+
   authenticated :user do
     root 'panel#dashboard'
-    get 'panel/plan'
   end
+
   unauthenticated :user do
     root 'static_pages#home'
   end
+
+  get 'panel/plan', to: 'panel#plan'
+
   post "stripe/webhook", to: "stripe_webhook#action"
+
+  resources :plans
+  resources :payments
+  resources :subscriptions, path_names: { new: 'new/:plan_id' }
+
 end
