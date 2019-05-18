077c2cd4b6d638ee327d2f72cb1c4da2ce5cd767
Andrew De Abreu Ruiz <andrewabrer@gmail.com>
2019-05-18 14:21:57 +0100

    Move responsability of start create subscription workflow to SubscriptionController

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,7 +1,17 @@
 class SubscriptionsController < ApplicationController
 
+  before_action :set_selected_plan, only: [:new, :create]
+
   def new
-    @paid_plan = Plan.find(params[:plan_id])
+  end
+
+  def create
+    workflow = stripe_subscription_workflow
+    if workflow.success
+      redirect_to root_path, notice: t('messages.subscriptions.subscribed', name: @selected_plan.name)
+    else
+      redirect_to new_subscription_path(@selected_plan.id), alert: workflow.error_message
+    end
   end
 
   def destroy
@@ -14,6 +24,26 @@ class SubscriptionsController < ApplicationController
     else
       redirect_to panel_plan_path, alert: t('messages.failure.not_unsubscribed')
     end
+  end
+
+  private def stripe_subscription_workflow
+    workflow = CreatesSubscriptionViaStripe.new(
+        user: current_user,
+        plan: @selected_plan,
+        token: StripeToken.new(**card_params))
+    workflow.run
+    workflow
+  end
+
+  private def card_params
+    params.permit(
+        :credit_card_number, :expiration_month,
+        :expiration_year, :cvc,
+        :stripe_token).to_h.symbolize_keys
+  end
+
+  private def set_selected_plan
+    @selected_plan = Plan.find(params[:plan_id])
   end
 
 end
