1dfee4c07d6c50d3685948bd0120fd38909a399a
Andrew De Abreu Ruiz <andrewabrer@gmail.com>
2019-05-22 23:01:53 +0100

    Fixed user reset password spec

--- spec/features/user_reset_password_spec.rb
+++ spec/features/user_reset_password_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.feature "User reset password", type: :feature do
 
 	scenario "with valid information" do
-		
+
 		create(:user, email: "test@example.com", password: "foobar")
 
 		visit root_path
 
-		click_link I18n.t('pages.header.navigation_links.signin')
+		click_link I18n.t('layouts.header.links.signin')
 		expect(current_path).to eq new_user_session_path
 
 		click_link I18n.t('devise.shared.links.forgot_your_password')
@@ -21,7 +21,7 @@ RSpec.feature "User reset password", type: :feature do
 		expect(page).to have_content I18n.t('devise.passwords.send_instructions')
 
   	open_email "test@example.com", with_subject: I18n.t('devise.mailer.reset_password_instructions.subject')
-  	visit_in_email I18n.t('devise.mailer.reset_password_instructions.action')		
+  	visit_in_email I18n.t('devise.mailer.reset_password_instructions.action')
 
   	expect(current_path).to eq edit_user_password_path
 
@@ -35,24 +35,24 @@ RSpec.feature "User reset password", type: :feature do
 	end
 
 	context "with invalid information" do
-		
+
 		scenario "with a non-existent email" do
-			
+
 			user = create(:user, email: "test@example.com", password: "foobar")
 
 			visit root_path
 
-			click_link I18n.t('pages.header.navigation_links.signin')
+			click_link I18n.t('layouts.header.links.signin')
 			expect(current_path).to eq new_user_session_path
 
 			click_link I18n.t('devise.shared.links.forgot_your_password')
 			expect(current_path).to eq new_user_password_path
 
 			fill_in "Email", with: "not-exists-test@example.com"
-			click_button I18n.t('devise.passwords.new.send_me_reset_password_instructions')		
+			click_button I18n.t('devise.passwords.new.send_me_reset_password_instructions')
 
 			expect(current_path).to eq user_password_path
-			expect(page).to have_content I18n.t('errors.messages.not_saved.one')				
+			expect(page).to have_content I18n.t('errors.messages.not_saved.one')
 
 		end
 
@@ -62,27 +62,27 @@ RSpec.feature "User reset password", type: :feature do
 
 			visit root_path
 
-			click_link I18n.t('pages.header.navigation_links.signin')
+			click_link I18n.t('layouts.header.links.signin')
 			expect(current_path).to eq new_user_session_path
 
 			click_link I18n.t('devise.shared.links.forgot_your_password')
 			expect(current_path).to eq new_user_password_path
 
 			expect(page).to have_field("Email", with: "", type: "email")
-			click_button I18n.t('devise.passwords.new.send_me_reset_password_instructions')		
+			click_button I18n.t('devise.passwords.new.send_me_reset_password_instructions')
 
 			expect(current_path).to eq user_password_path
-			expect(page).to have_content I18n.t('errors.messages.not_saved.one')				
-			
+			expect(page).to have_content I18n.t('errors.messages.not_saved.one')
+
 		end
 
 		scenario "with a blank password" do
-			
+
 			create(:user, email: "test@example.com", password: "foobar")
 
 			visit root_path
 
-			click_link I18n.t('pages.header.navigation_links.signin')
+			click_link I18n.t('layouts.header.links.signin')
 			expect(current_path).to eq new_user_session_path
 
 			click_link I18n.t('devise.shared.links.forgot_your_password')
@@ -95,16 +95,16 @@ RSpec.feature "User reset password", type: :feature do
 			expect(page).to have_content I18n.t('devise.passwords.send_instructions')
 
 	  	open_email "test@example.com", with_subject: I18n.t('devise.mailer.reset_password_instructions.subject')
-	  	visit_in_email I18n.t('devise.mailer.reset_password_instructions.action')		
+	  	visit_in_email I18n.t('devise.mailer.reset_password_instructions.action')
 
 	  	expect(current_path).to eq edit_user_password_path
 
       expect(find_field("New password", type: "password").value).to be_nil
-      expect(find_field("Confirm new password", type: "password").value).to be_nil  
+      expect(find_field("Confirm new password", type: "password").value).to be_nil
 			click_button I18n.t('devise.passwords.edit.change_my_password')
 
 	  	expect(current_path).to eq user_password_path
-			expect(page).to have_content I18n.t('errors.messages.not_saved.one')	
+			expect(page).to have_content I18n.t('errors.messages.not_saved.one')
 
 		end
 
@@ -114,7 +114,7 @@ RSpec.feature "User reset password", type: :feature do
 
 			visit root_path
 
-			click_link I18n.t('pages.header.navigation_links.signin')
+			click_link I18n.t('layouts.header.links.signin')
 			expect(current_path).to eq new_user_session_path
 
 			click_link I18n.t('devise.shared.links.forgot_your_password')
@@ -127,17 +127,17 @@ RSpec.feature "User reset password", type: :feature do
 			expect(page).to have_content I18n.t('devise.passwords.send_instructions')
 
 	  	open_email "test@example.com", with_subject: I18n.t('devise.mailer.reset_password_instructions.subject')
-	  	visit_in_email I18n.t('devise.mailer.reset_password_instructions.action')		
+	  	visit_in_email I18n.t('devise.mailer.reset_password_instructions.action')
 
 	  	expect(current_path).to eq edit_user_password_path
 
       fill_in "New password", with: "123456"
-      expect(find_field("Confirm new password", type: "password").value).to be_nil 
+      expect(find_field("Confirm new password", type: "password").value).to be_nil
 			click_button I18n.t('devise.passwords.edit.change_my_password')
 
 	  	expect(current_path).to eq user_password_path
-			expect(page).to have_content I18n.t('errors.messages.not_saved.one')	
-			
+			expect(page).to have_content I18n.t('errors.messages.not_saved.one')
+
 		end
 
 	end
