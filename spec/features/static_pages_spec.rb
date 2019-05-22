4fbf5f123eb446d161c6061eefa1bd3d4e1c0d00
Andrew De Abreu Ruiz <andrewabrer@gmail.com>
2019-05-22 23:07:00 +0100

    Fixed static pages spec

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.feature "StaticPages", type: :feature do
-  
+
 	scenario "viewing the home page" do
 		visit root_path
 
-		expect(page).to have_title("Inicio | Box2Share")
-		
-		expect(page).to have_link(I18n.t('pages.header.navigation_links.home'), :href => root_path(locale: I18n.locale))
-		expect(page).to have_link(I18n.t('pages.header.navigation_links.pricing'), :href => '#')	
-		expect(page).to have_link(I18n.t('pages.header.navigation_links.signin'), :href => new_user_session_path(locale: I18n.locale))	
-		expect(page).to have_link(I18n.t('pages.header.navigation_links.signup'), :href => new_user_registration_path(locale: I18n.locale))
+		expect(page).to have_title("#{I18n.t('static_pages.home.title')} | Box2Share")
+
+		expect(page).to have_link(I18n.t('layouts.header.links.home'), href: root_path(locale: I18n.locale))
+		expect(page).to have_link(I18n.t('layouts.header.links.pricing'), href: plans_path(locale: I18n.locale))
+		expect(page).to have_link(I18n.t('layouts.header.links.signin'), href: new_user_session_path(locale: I18n.locale))
+		expect(page).to have_link(I18n.t('layouts.header.links.signup'), href: new_user_registration_path(locale: I18n.locale))
 	end
 
 end
