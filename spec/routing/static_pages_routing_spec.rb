6d913a9975657ea840a021d17a46a236fc884f84
Andrew De Abreu Ruiz <andrewabrer@gmail.com>
2019-05-23 21:48:21 +0100

    Delete static pages routing spec

--- spec/routing/static_pages_routing_spec.rb
+++
@@ -1,9 +0,0 @@
-require "rails_helper"
-
-RSpec.describe "routes for StaticPagesController", :type => :routing do
-
-  it "routes / to the StaticPages controller" do
-    expect(get("/")).to route_to("static_pages#home")
-  end
-
-end
