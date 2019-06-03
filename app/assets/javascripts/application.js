430a9dde2fe319496ed59a01e08774cf676e8ddb
Andrew De Abreu Ruiz <andrewabrer@gmail.com>
2019-06-03 19:44:08 +0100

    Quit white breadcrumb background color

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,38 +19,4 @@
 //= require datatables
 //= require_tree .
 
-$(document).on('turbolinks:load', function(){
-  bsCustomFileInput.init()
-})
 
-$(document).on('turbolinks:load', function(){
-  $("#files").dataTable({
-    "columnDefs": [
-      { "targets": 0, "orderable": false}
-    ],
-    "language": {
-      "sProcessing":     "Procesando...",
-      "sLengthMenu":     "Mostrar _MENU_ registros",
-      "sZeroRecords":    "No se encontraron resultados",
-      "sEmptyTable":     "Ningún dato disponible en esta tabla",
-      "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-      "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
-      "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
-      "sInfoPostFix":    "",
-      "sSearch":         "Buscar:",
-      "sUrl":            "",
-      "sInfoThousands":  ",",
-      "sLoadingRecords": "Cargando...",
-      "oPaginate": {
-        "sFirst":    "Primero",
-        "sLast":     "Último",
-        "sNext":     "Siguiente",
-        "sPrevious": "Anterior"
-      },
-      "oAria": {
-        "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
-        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-      }
-    }
-  });
-});
