b11dc0005a3f07277640e448343df2fda63a9c21
Andrew De Abreu Ruiz <andrewabrer@gmail.com>
2019-06-03 16:33:18 +0100

    Create temporal Assets controller

---
+++ app/controllers/assets_controller.rb
@@ -0,0 +1,61 @@
+class AssetsController < ApplicationController
+  before_action :set_asset, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+
+  def index
+    @assets = current_user.assets
+  end
+
+  # def show
+  # end
+
+  def new
+    @asset = current_user.assets.build
+  end
+
+  # def edit
+  # end
+
+  def create
+    @asset = current_user.assets.build(asset_params)
+
+    respond_to do |format|
+      if @asset.save
+        format.html { redirect_to assets_path, notice: 'El archivo se ha subido correctamente.' }
+        #format.json { render :show, status: :created, location: @asset }
+      else
+        format.html { render :new }
+        #format.json { render json: @asset.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # def update
+  #   respond_to do |format|
+  #     if @asset.update(asset_params)
+  #       format.html { redirect_to @asset, notice: 'Asset was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @asset }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @asset.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+
+  # def destroy
+  #   @asset.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to assets_url, notice: 'Asset was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
+
+  private
+    def set_asset
+      @asset = current_user.assets.find(params[:id])
+    end
+
+    def asset_params
+      params.require(:asset).permit(:uploaded_file)
+    end
+end
