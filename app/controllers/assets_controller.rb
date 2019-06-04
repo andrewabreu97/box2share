e6971e90c0985459d3dab3c71779cd7f0ca0fc3d
Andrew De Abreu Ruiz <andrewabrer@gmail.com>
2019-06-04 18:41:58 +0100

    Clear Asset resource files

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -8,49 +8,32 @@ class AssetsController < ApplicationController
     @assets = current_user.assets
   end
 
-  # def show
-  # end
+  def show
+  end
 
   def new
     @asset = current_user.assets.build
   end
 
-  # def edit
-  # end
+  def edit
+  end
 
   def create
     @asset = current_user.assets.build(asset_params)
-
-    respond_to do |format|
-      if @asset.save
-        format.html { redirect_to panel_files_path, notice: 'El archivo se ha subido correctamente.' }
-        #format.json { render :show, status: :created, location: @asset }
-      else
-        format.html { render :new }
-        #format.json { render json: @asset.errors, status: :unprocessable_entity }
-      end
+    if @asset.save
+      redirect_to panel_files_path, notice: 'El archivo se ha subido correctamente.'
+    else
+      render :new
     end
   end
 
-  # def update
-  #   respond_to do |format|
-  #     if @asset.update(asset_params)
-  #       format.html { redirect_to @asset, notice: 'Asset was successfully updated.' }
-  #       format.json { render :show, status: :ok, location: @asset }
-  #     else
-  #       format.html { render :edit }
-  #       format.json { render json: @asset.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
+  def update
+  end
 
   def destroy
     @asset.uploaded_file.purge
     @asset.destroy
-    respond_to do |format|
-      format.html { redirect_to panel_files_path, notice: 'El archivo se ha eliminado correctamente.' }
-      #format.json { head :no_content }
-    end
+    redirect_to panel_files_path, notice: 'El archivo se ha eliminado correctamente.'
   end
 
   def download
