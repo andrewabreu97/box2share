1117155ce4f26245fd0cb48861f28729b0dbfc71
Andrew De Abreu Ruiz <andrewabrer@gmail.com>
2019-06-05 19:35:01 +0100

    Add file icons to files view in panel

--- app/helpers/panel_helper.rb
+++ app/helpers/panel_helper.rb
@@ -1,3 +1,11 @@
 module PanelHelper
 
+  def file_icon(extension)
+    if extension && FileTest.exists?(Rails.root.join('app','assets','images','fileicons', "#{extension.downcase}.png"))
+      "fileicons/#{extension.downcase}.png"
+    else
+      'fileicons/file.png'
+    end
+  end
+
 end
