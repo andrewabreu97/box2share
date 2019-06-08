8026e4e6e1ce476168a1ba84bc8d2718241a671b
Andrew De Abreu Ruiz <andrewabrer@gmail.com>
2019-06-08 19:58:26 +0100

    Difference between Stripe production and development keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,3 +1,7 @@
-Stripe.api_key = Rails.application.secrets.stripe_secret_key
+if Rails.env.production?
+  Stripe.api_key = ENV['STRIPE_PRODUCTION_SECRET_KEY']
+else
+  Stripe.api_key = Rails.application.secrets.stripe_secret_key
+end
 #raise "Missing Stripe API Key" unless Stripe.api_key
 STRIPE_JS_HOST = "https://js.stripe.com".freeze unless defined? STRIPE_JS_HOST
