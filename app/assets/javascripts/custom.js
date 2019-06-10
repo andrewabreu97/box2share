a13432582c6ed4249d987b4b8253929f17c17b71
Andrew De Abreu Ruiz <andrewabrer@gmail.com>
2019-06-10 19:25:14 +0100

    Add jQuery code to payments datatable in custom.js file

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,11 +1,5 @@
 $(document).on('turbolinks:load', function(){
 
-  // $(".custom-file-input").on("change", function() {
-  //   alert("Hola")
-  //   var fileName = $(this).val().split("\\").pop();
-  //   $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
-  // });
-
   bsCustomFileInput.init()
 
   $("#files").dataTable({
@@ -40,6 +34,37 @@ $(document).on('turbolinks:load', function(){
   });
 
   $("#folder").dataTable({
+    "order": [[ 1, "asc" ]],
+    "columnDefs": [
+      { "targets": [0,4], "orderable": false, "searchable": false}
+    ],
+    "language": {
+      "sProcessing":     "Procesando...",
+      "sLengthMenu":     "Mostrar _MENU_ registros",
+      "sZeroRecords":    "No se encontraron resultados",
+      "sEmptyTable":     "Ningún dato disponible en esta tabla",
+      "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+      "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+      "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+      "sInfoPostFix":    "",
+      "sSearch":         "Buscar:",
+      "sUrl":            "",
+      "sInfoThousands":  ",",
+      "sLoadingRecords": "Cargando...",
+      "oPaginate": {
+        "sFirst":    "Primero",
+        "sLast":     "Último",
+        "sNext":     "Siguiente",
+        "sPrevious": "Anterior"
+      },
+      "oAria": {
+        "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+      }
+    }
+  });
+
+  $("#payments").dataTable({
     "order": [[ 1, "asc" ]],
     "columnDefs": [
       { "targets": [0,4], "orderable": false, "searchable": false}
