51fe7e8891f0f4cf90410b19df4847ddbf942ee7
Andrew De Abreu Ruiz <andrewabrer@gmail.com>
2019-06-12 22:59:42 +0100

    Put limit in size file when upload it

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -4,7 +4,7 @@ class Asset < ApplicationRecord
 
   has_one_attached :uploaded_file
 
-  validates :uploaded_file, attached: true
+  validates :uploaded_file, attached: true, size: { less_than: 5.gigabytes, message: "El tamaño del fichero debe ser menor a 5 gigabytes." }
   validates_presence_of :name
   validates_uniqueness_of :name, scope: [:folder_id, :user_id]
 end
