d829116edb030ea33ffc0ae30d23557cbed12817
Andrew De Abreu Ruiz <andrewabrer@gmail.com>
2019-06-12 18:52:36 +0100

    Put folder and root assets jQuery Datatable in one

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,17 +2,7 @@ $(document).on('turbolinks:load', function(){
 
   bsCustomFileInput.init()
 
-  $("#files").dataTable({
-    "order": [[ 1, "asc" ]],
-    "columnDefs": [
-      { "targets": [0,4], "orderable": false, "searchable": false}
-    ],
-    "language": {
-      "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Spanish.json"
-    }
-  });
-
-  $("#folder").dataTable({
+  $("table.files").dataTable({
     "order": [[ 1, "asc" ]],
     "columnDefs": [
       { "targets": [0,4], "orderable": false, "searchable": false}
