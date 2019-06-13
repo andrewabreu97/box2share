7febf2243cca3d967ae28ef641db223dc4bf7a47
Andrew De Abreu Ruiz <andrewabrer@gmail.com>
2019-06-13 22:35:43 +0100

    Change region in bucket and change name of credentials

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,13 +7,13 @@ if Rails.env.production?
 
     config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key  => ENV['S3_SECRET_KEY'],
-      :region                 => 'eu-west-2',
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
+      :region                 => 'us-east-1'
   #    :host                   => 's3.example.com',
-      :endpoint               => 'https://s3.eu-west-2.amazonaws.com/'
+  #    :endpoint               => 'https://s3.eu-west-2.amazonaws.com/'
     }
-    config.fog_directory  = ENV['S3_AVATAR_BUCKET']
+    config.fog_directory  = ENV['S3_BUCKET']
     config.fog_public     = false
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
   end
