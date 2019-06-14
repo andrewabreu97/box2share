aae573d20d2854bb00dfdc21ebcbb4b91e964099
Andrew De Abreu Ruiz <andrewabrer@gmail.com>
2019-06-14 18:03:49 +0100

    Clear service_statistics panel controller action method

--- app/controllers/admin/panel_controller.rb
+++ app/controllers/admin/panel_controller.rb
@@ -3,12 +3,6 @@ class Admin::PanelController < Admin::ApplicationController
   layout 'panel'
 
   def service_statistics
-    @users_count = User.count
-    @folders_count = Folder.count
-    @assets_count = Asset.count
-    @uploaded_assets_count = User.sum(:uploaded_assets_count)
-    @downloaded_assets_count = User.sum(:downloaded_assets_count)
-    @shared_assets_count = User.sum(:shared_assets_count)
 
   end
 
