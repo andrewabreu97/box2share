e04a3453c5f9e36dd0b89c92f35f2ccc98d95fb5
Andrew De Abreu Ruiz <andrewabrer@gmail.com>
2019-06-14 19:54:40 +0100

    Add delete user functionality base

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,18 @@
 class Admin::UsersController < Admin::ApplicationController
+  before_action :require_existing_user, only: [:destroy]
 
   def index
-    @users = User.all
+    @users = User.where(admin: false)
   end
+
+  def destroy
+  end
+
+  private
+    def require_existing_user
+      @user = User.find(params[:id])
+    rescue
+      redirect_to admin_users_path, alert: "Este usuario no existe o ya ha sido eliminado."
+    end
 
 end
